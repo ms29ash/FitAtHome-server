11a17d84c62de7e30d4c92ec5a555d83f921592a
ms29ash <ms29ash@gmail.com>
2022-06-30 12:47:35 +0530

    add findBy id food

--- routes/food.js
+++ routes/food.js
@@ -6,19 +6,47 @@ const Meals = require('../models/Meals')
 const Reviews = require('../models/Reviews')
 
 /* GET home page. */
+
 router.get('/food', async (req, res, next) => {
-  let food = await Food.find({});
-  res.json({ food: food });
+  try {
+    let food = await Food.find();
+    res.json({ food });
+  } catch (error) {
+    console.log(error)
+  }
+
+});
+router.get('/food/:id', async (req, res, next) => {
+  try {
+    const id = req.params.id;
+
+    let food = await Food.findOne({ id: id });
+    res.json({ food });
+  } catch (error) {
+    console.log(error)
+  }
+
 });
 
 
 router.get('/meals', async (req, res, next) => {
-  let meals = await Meals.find({});
-  res.json({ meals: meals });
+  try {
+    let meals = await Meals.find({});
+    res.json({ meals });
+  } catch (error) {
+    console.log(error)
+  }
+
 });
 router.get('/reviews', async (req, res, next) => {
-  let reviews = await Reviews.find({});
-  res.json({ reviews: reviews });
+  try {
+
+
+    let reviews = await Reviews.find({});
+    res.json({ reviews });
+  } catch (error) {
+    console.log(error)
+  }
 });
 
 
