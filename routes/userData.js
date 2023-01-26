b13eb2c742865e8b76573a6634320c3f436523a8
ms29ash <ms29ash@gmail.com>
2023-01-26 22:46:52 +0530

    Create Add Address endpoint

--- routes/userData.js
+++ routes/userData.js
@@ -6,15 +6,19 @@ const User = require('../models/User.js');
 const fetchIds = require('../middleware/verifyUser.js');
 
 /* GET home page. */
-router.get('/', fetchIds, async (req, res, next) => {
+router.put('/addAddress', fetchIds, async (req, res, next) => {
     try {
-        let id = req.user
-        let user = await User.find({ _id: id });
-        return res.status(200).send({ address: user.address })
+        let { id } = req.user?.user
+        const user = await User.findOne({ _id: id })
+
+        user.userData.address = req.body.address
+        await user.save()
+
+        return res.status(201).json({ success: true, message: 'Address updated successfully' })
 
     } catch (error) {
         console.log(error)
-        return res.status(500).send({ success: false, message: "Internal server error" })
+        return res.status(500).send({ success: false, errorMessage: "Internal server error" })
     }
 });
 
