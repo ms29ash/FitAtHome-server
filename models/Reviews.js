f64270c5bfe1e095fb5f920df97c9a4ceacc8f81
ms29ash <ms29ash@gmail.com>
2023-10-22 08:00:50 +0530

    fix review model

--- models/Reviews.js
+++ models/Reviews.js
@@ -1,11 +1,23 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const foodSchema = new Schema({
-    name: String, // String is shorthand for {type: String}
-    review: String,
-    Date: Date,
-});
-const Review = mongoose.model('Review', foodSchema);
+const foodSchema = new Schema(
+  {
+    name: String,
+    content: String,
+    image: {
+      type: String,
+      default:
+        "https://images.pexels.com/photos/1132040/pexels-photo-1132040.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    },
+  },
+  {
+    timestamps: {
+      createdAt: true,
+      updatedAt: true,
+    },
+  }
+);
+const Review = mongoose.model("Review", foodSchema);
 
 module.exports = Review;
